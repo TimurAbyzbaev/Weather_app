733bfe57435e8ee0ea6b0acbc8de38abacb60eb3
Timur <timur@vlaga.pro>
2022-10-12 14:37:10 +0500

    Использовал ViewBinding \ потестил разные варианции

--- app/src/main/java/ru/abyzbaev/weather_app/MainActivity.kt
+++ app/src/main/java/ru/abyzbaev/weather_app/MainActivity.kt
@@ -2,10 +2,19 @@ package ru.abyzbaev.weather_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import ru.abyzbaev.weather_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.myRoot)
+        binding.tv.setText("Hello THERE")
+
     }
 }
